bfd43fe0e5c1f970f2243600bfcb0ec134436d53
keelerjr12 <keelerjr12@gmail.com>
2019-10-25 13:29:27 -0500

    Added

---
+++ RioSuaveAPI/MailingList/MailingListController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RioSuaveLib;
+
+namespace RioSuaveAPI.MailingList
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MailingListController : ControllerBase
+    {
+        public MailingListController(ILogger<MailingListController> logger, EmailService emailService)
+        {
+            _logger = logger;
+            _emailService = emailService;
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]Email email)
+        {
+            _logger.LogInformation(email.Address);
+            _emailService.Send("keelerwebdev@gmail.com", email.Address, "Mailing List Request For: " + email.Address, email.Address + " would like to join the mailing list.");
+        }
+
+        private readonly ILogger<MailingListController> _logger;
+        private readonly EmailService _emailService;
+    }
+}
---
+++ RioSuaveLib/JWT/JwtService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+
+namespace RioSuaveLib.JWT
+{
+    public class JwtService
+    {
+        public JwtService(string issuer, string audience, SymmetricSecurityKey signingKey)
+        {
+            _issuer = issuer;
+            _audience = audience;
+            _signingKey = signingKey;
+        }
+
+        public JwtDTO Generate()
+        {
+            var header = new JwtHeader(new SigningCredentials(_signingKey, SecurityAlgorithms.HmacSha256));
+            var payload = new JwtPayload(_issuer, _audience, null, DateTime.UtcNow, DateTime.Now.AddHours(6));
+
+            var jwt = new JwtSecurityToken(header, payload);
+            var encryptedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            return new JwtDTO { Token = encryptedJwt };
+        }
+
+        private readonly string _issuer;
+        private readonly string _audience;
+        private readonly SymmetricSecurityKey _signingKey;
+    }
+}
